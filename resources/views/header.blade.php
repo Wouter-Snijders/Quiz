b3e591e969fa73e44ad9174c97d32d0ca1368229
Wouter Snijders <145758873+Wouter-Snijders@users.noreply.github.com>
2025-09-07 23:26:29 +0200

    Header toevoegen

---
+++ resources/views/header.blade.php
@@ -0,0 +1,12 @@
+<header class="bg-black text-white">
+    <nav class="container mx-auto flex items-center justify-between py-4">
+        <div class="text-lg font-bold">
+            <a href="/" class="hover:underline">Quiz Applicatie</a>
+        </div>
+        <div class="flex space-x-6">
+            <a href="#" class="hover:bg-gray-700 px-3 py-2 rounded transition">Home</a>
+            <a href="#" class="hover:bg-gray-700 px-3 py-2 rounded transition">Quizzen</a>
+            <a href="#" class="hover:bg-gray-700 px-3 py-2 rounded transition">Contact</a>
+        </div>
+    </nav>
+</header>
